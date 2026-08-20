102a5dc9a98d61094e5f34437d621af772909faa
Thibaut Coustillet <thib.coustillet@gmail.com>
2026-08-20 12:34:26 +0200

    style(footer): add Github logo and link

--- js/site-footer.js
+++ js/site-footer.js
@@ -2,7 +2,13 @@ class SiteFooter extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <footer class="footer">
-        <p>&copy; 2026 &middot; Thibaut Coustillet &middot; All rights reserved</p>
+        <p>
+        &copy; 2026 &middot; Thibaut Coustillet
+        <a href="https://github.com/tcoustillet/" target="_blank" rel="noopener noreferrer" class="footer-github-link">
+        <img src="assets/GitHub_Invertocat_White.svg" alt="Github" width="16" height="16">
+        </a>
+        &middot; All rights reserved
+        </p>
     </footer>
     `;
   }
